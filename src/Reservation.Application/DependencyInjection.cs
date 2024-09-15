036f522e6bad4401e68914c8123837d30197813f
Pedro William B Moraes <william.baiardi@hotmail.com>
2024-09-15 18:14:45 -0300

    feat(Reservation.Application): Add MediatR dependency injection setup

---
+++ src/Reservation.Application/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Reservation.Domain.Bookings;
+
+namespace Reservation.Application;
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(configuration =>
+        {
+            configuration.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly);
+        });
+
+        services.AddTransient<PricingService>();
+
+        return services;
+    }
+}
