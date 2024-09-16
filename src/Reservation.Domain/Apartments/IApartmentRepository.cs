285b3a562e93fca4c29980442e5a4270ff29c0b5
Pedro William B Moraes <william.baiardi@hotmail.com>
2024-09-15 21:05:58 -0300

    refactor(ApartmentRepository): Rename GetById to GetByIdAsync

--- src/Reservation.Domain/Apartments/IApartmentRepository.cs
+++ src/Reservation.Domain/Apartments/IApartmentRepository.cs
@@ -1,5 +1,5 @@
 ﻿namespace Reservation.Domain.Apartments;
 public interface IApartmentRepository
 {
-    Task<Apartment?> GetById(Guid id, CancellationToken cancellationToken = default);
+    Task<Apartment?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
 }
