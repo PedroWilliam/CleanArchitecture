ea5e0bcc9148b873e558fedad28a218b16c48751
Pedro William B Moraes <william.baiardi@hotmail.com>
2024-09-16 21:56:35 -0300

    feat(Reservation.Infrastructure): Add SqlConnectionFactory and DateOnlyTypeHandler

---
+++ src/Reservation.Infrastructure/Data/SqlConnectionFactory.cs
@@ -0,0 +1,22 @@
+﻿using Npgsql;
+using Reservation.Application.Abstractions.Data;
+using System.Data;
+
+namespace Reservation.Infrastructure.Data;
+internal sealed class SqlConnectionFactory : ISqlConnectionFactory
+{
+    private readonly string _connectionString;
+
+    public SqlConnectionFactory(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public IDbConnection CreateConnection()
+    {
+        var connection = new NpgsqlConnection(_connectionString);
+        connection.Open();
+
+        return connection;
+    }
+}
--- src/Reservation.Infrastructure/DependencyInjection.cs
+++ src/Reservation.Infrastructure/DependencyInjection.cs
@@ -1,12 +1,15 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Dapper;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Reservation.Application.Abstractions.Data;
 using Reservation.Application.Abstractions.Email;
 using Reservation.Application.Abstractions.Provider;
 using Reservation.Domain.Abstractions;
 using Reservation.Domain.Apartments;
 using Reservation.Domain.Bookings;
 using Reservation.Domain.Users;
+using Reservation.Infrastructure.Data;
 using Reservation.Infrastructure.Email;
 using Reservation.Infrastructure.Providers;
 using Reservation.Infrastructure.Repositories;
@@ -32,6 +35,11 @@ public static class DependencyInjection
         services.AddScoped<IApartmentRepository, ApartmentRepository>();
         services.AddScoped<IBookingRepository, BookingRepository>();
         services.AddScoped<IUnitOfWork>(sp=>sp.GetRequiredService<ApplicationDbContext>());
+
+        services.AddSingleton<ISqlConnectionFactory>(_ =>
+            new SqlConnectionFactory(connectionString));
+
+        SqlMapper.AddTypeHandler(new DateOnlyTypeHandler());
 
         return services;
     }
