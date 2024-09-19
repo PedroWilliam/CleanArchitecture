eb4320f60f3cb5232dcd6a23881110d08de34cc9
Pedro William B Moraes <william.baiardi@hotmail.com>
2024-09-18 23:44:35 -0300

    feat(Authentication): Add user context and authorization service

---
+++ src/Reservation.Infrastructure/Authorization/UserRolesResponse.cs
@@ -0,0 +1,9 @@
+﻿using Reservation.Domain.Users;
+
+namespace Reservation.Infrastructure.Authorization;
+public class UserRolesResponse
+{
+    public Guid UserId { get; init; }
+
+    public List<Role> Roles { get; init; } = [];
+}
