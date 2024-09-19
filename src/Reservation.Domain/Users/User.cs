d53b813bc53e26838af8812348c241cf96662a2c
Pedro William B Moraes <william.baiardi@hotmail.com>
2024-09-18 22:13:25 -0300

    feat(User): Add IdentityId to User class and its migration

--- src/Reservation.Domain/Users/User.cs
+++ src/Reservation.Domain/Users/User.cs
@@ -26,6 +26,7 @@ public class User : Entity
     public LastName LastName { get; private set; }
 
     public Email Email { get; private set; }
+    public string IdentityId { get; private set; } = string.Empty;
 
     public static User Create(FirstName firstName, LastName lastName, Email email)
     {
@@ -34,5 +35,10 @@ public class User : Entity
         user.RaiseDomainEvent(new UserCreatedDomainEvent(user.Id));
 
         return user;
+    }
+
+    public void SetIdentityId(string identityId)
+    {
+        IdentityId = identityId;
     }
 }
