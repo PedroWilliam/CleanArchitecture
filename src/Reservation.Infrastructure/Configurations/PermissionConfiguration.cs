515e87de993b80a1d04f3e7817a05a9495b201ef
Pedro William B Moraes <william.baiardi@hotmail.com>
2024-09-19 00:11:24 -0300

    feat(User Permissions): Add permission and role-permission configurations

---
+++ src/Reservation.Infrastructure/Configurations/PermissionConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Reservation.Domain.Users;
+
+namespace Reservation.Infrastructure.Configurations;
+internal sealed class PermissionConfiguration : IEntityTypeConfiguration<Permission>
+{
+    public void Configure(EntityTypeBuilder<Permission> builder)
+    {
+        builder.ToTable("permissions");
+
+        builder.HasKey(permission => permission.Id);
+
+        builder.HasData(Permission.UsersRead);
+    }
+}
--- src/Reservation.Infrastructure/Migrations/20240919025408_App_Permissions.Designer.cs
+++ src/Reservation.Infrastructure/Migrations/20240919025408_App_Permissions.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Reservation.Infrastructure;
@@ -11,9 +12,11 @@ using Reservation.Infrastructure;
 namespace Reservation.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240919025408_App_Permissions")]
+    partial class App_Permissions
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
