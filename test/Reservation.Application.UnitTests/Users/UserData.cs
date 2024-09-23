85c80b15269b89fe554950dfcd9b3765bf8393c7
Pedro William B Moraes <william.baiardi@hotmail.com>
2024-09-22 22:52:40 -0300

    test(Reservation): Add unit tests for Reservation application

---
+++ test/Reservation.Application.UnitTests/Users/UserData.cs
@@ -0,0 +1,12 @@
+﻿using Reservation.Domain.Users;
+
+namespace Reservation.Application.UnitTests.Users;
+internal static class UserData
+{
+    public static User Create() => User.Create(FirstName, LastName, Email);
+
+    public static readonly FirstName FirstName = new("First");
+    public static readonly LastName LastName = new("Last");
+    public static readonly Email Email = new("test@reservation.com");
+}
+
